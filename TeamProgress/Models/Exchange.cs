2098a8c45c013a8503b09be8a24f92cf7e541cd9
Regis Charlot <Regis@imo-online.com>
2015-06-11 16:11:44 -0500

    changed exchange tab to include address as a link to google map

--- TeamProgress/Models/Exchange.cs
+++ TeamProgress/Models/Exchange.cs
@@ -15,6 +15,28 @@ namespace TeamProgress.Models
         public string ZIP { get; set; }
         public int Van { get; set; }
 
+        public string MapAddress { get
+        {
+            return string.Format("{0}, {1}, {2}, {3} {4}", new object[] {Name, Address, City, State, ZIP});
+        } }
+
+        public string GoogleMapLink
+        {
+            get
+            {
+                string s = string.Format("{0}, {1}, {2} {3}", new object[] { Address, City, State, ZIP });
+                return string.Format("<a target=\"_blank\" href=\"https://www.google.com/maps/place/{0}\">{1}</a>", new object[] { s.Replace(" ", "+"), s });
+            }
+        }
+
+        public string FollowingLeg
+        {
+            get
+            {
+               return string.Format("<a href=\"https://www.ragnarrelay.com/race/chicago/legs/{0}\" target=\"_blank\">Leg {0}</a>", Id);
+            }
+        }
+
         public Exchange(int id, string name, string address, string city, string state, string zip, int van)
         {
             Id = id;
